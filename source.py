2f0a6ce9ceddffb7147fe4b0a9a66583db817f69
Gabriel Queiroz <gqmv@cin.ufpe.br>
2021-03-20 20:53:39 -0300

    Create source.py

---
+++ source.py
@@ -0,0 +1,36 @@
+import requests
+import random
+
+class user:
+    def __init__(self, name):
+        self.name = name
+        self.wins = 0
+
+    def increase(self):
+        self.wins += 1
+
+USERNAME = "" # Insert the streamer's username here.
+
+request = requests.get(f"https://tmi.twitch.tv/group/user/{USERNAME}/chatters")
+viewers = []
+for group in request.json()["chatters"]:
+    for viewer in request.json()["chatters"][group]:
+        viewers.append(user(viewer))
+
+print(f"Viewers que estão participando do sorteio:")
+for viewer in viewers: print(viewer.name)
+
+input("Para sortear, aperte enter.")
+while True:
+    winner = viewers[random.randint(0, len(viewers) - 1)]
+    print(f"O vencedor é {winner.name}, ele já foi sorteado outras {winner.wins} vezes.")
+
+    winner.increase()
+
+    prompt = input("Para sortear novamente, aperte enter. Para sair, aperte N seguido de enter.").lower()
+
+    if prompt == "n":
+        break
+
+print("Obrigado por participar :D")
+print("</> por Gabriel Queiroz")
